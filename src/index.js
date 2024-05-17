6c6d4a582b9028cdb833577138615ae3259c2504
Finisus <finisvisus@gmail.com>
2024-05-17 23:16:48 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import "7.css/dist/7.css";
+import "./style.css";
+
+// Header
+const header = document.querySelector('header');
+
+// Main
+const main = document.querySelector('main');
+
+const tickerH1 = document.createElement('h1');
+tickerH1.innerHTML = `$CHOMIK`;
+main.appendChild(tickerH1);
+
+// Footer
+const footer = document.querySelector('footer');
